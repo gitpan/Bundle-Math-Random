6ce3149410370b455669d4a572fa3f00da58a566
Steffen Müller <smueller@cpan.org>
2004-04-05 12:11:06 +0000

    Import of SMUELLER/Bundle-Math-Random-1.00 from CPAN.
    gitpan-cpan-distribution: Bundle-Math-Random
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         SMUELLER/Bundle-Math-Random-1.00.tar.gz
    gitpan-cpan-author:       SMUELLER
    gitpan-cpan-maturity:     released

---
+++ Random.pm
@@ -0,0 +1,50 @@
+package Bundle::Math::Random;
+use strict;
+use warnings;
+
+$VERSION = '1.00';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::Math::Random - Bundle of modules related to random numbers
+
+=head1 SYNOPSIS
+
+ perl -MCPAN -e 'install Bundle::Math::Random'
+
+=head1 CONTENTS
+
+Math::LogRand
+
+Math::Rand48
+
+Math::Random
+
+Math::Random::MT
+
+Math::Random::TT800
+
+Math::RandomOrg
+
+Math::RandomPrime
+
+Math::TrulyRandom
+
+=head1 DESCRIPTION
+
+This is a bundle of modules related to random numbers.
+
+Please have a look at L<Bundle::Math>.
+
+If you would like to see a specific module included in a future version of
+this bundle, please send me an email or use rt.cpan.org.
+
+=head1 AUTHOR
+
+Steffen Mueller E<lt>bundlemath-module at steffen-mueller dot netE<gt>
+
+=cut 
